7dedb16fc3c4b2af721c41f9416d419dd46c8521
taycannon <taycannon2003@gmail.com>
2023-10-02 19:24:01 -0500

    Updated HTML with weather info, CSS with looks, JS with the function as well as the saved data of previous city with no refresh

--- script.js
+++ script.js
@@ -1,56 +1,70 @@
-var apiKey = "875af71136649c184bc2e5c69a8a3db8"
-var form = document.getElementById("cityForm")
-var cityInput = document.getElementById("cityInput")
-var currentWeatherContainer = document.getElementsByClassName("currentWeatherContainer")
-var cityValue;
+var apiKey = "875af71136649c184bc2e5c69a8a3db8";
+var form = document.getElementById("cityForm");
+var cityInput = document.getElementById("cityInput");
+var cityNameElement = document.querySelector(".city-name");
+var weatherDescriptionElement = document.querySelector(".weather-description");
+var temperatureElement = document.querySelector(".temperature");
+var humidityElement = document.querySelector(".humidity");
 
-function currentWeatherData(cityData) {
-    var lon = cityData[0].lon
-    var lat = cityData[0].lat
-    var cityName = cityData[0].name
-    console.log(lon, lat, cityName);
-    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    handleSearchInput();
+    cityInput.value = '';
+});
 
-
-    fetch(apiURL).then(function (response) {
-        return response.json()
-    }).then(function (data) {
-        console.log(data);
-        renderCurrentWeatherData(data);
-    })
+function handleSearchInput() {
+    var city = cityInput.value;
+    console.log(city);
+    getCoords(city);
 }
 
 function getCoords(cityInput) {
-    var apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=5&appid=${apiKey}`
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`;
+
     fetch(apiURL)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data);
-        currentWeatherData(data)
+        currentWeatherData(data);
     })
     .catch(function (error) {
         console.log(error);
     });
 }
 
-form.addEventListener("submit", function (e) {
-    e.stopPropagation();
-    e.preventDefault();
-    handleSearchInput();
-    cityInput.value = '';
-}) 
+function currentWeatherData(cityData) {
+    var lon = cityData.coord.lon;
+    var lat = cityData.coord.lat;
+    var cityName = cityData.name;
+    console.log(lon, lat, cityName);
+    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
-function handleSearchInput() {
-    let city = cityInput.value;
-    console.log(city);
-    getCoords(city);
-    cityValue = city
+    fetch(apiURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        renderCurrentWeatherData(data);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
 }
 
 function renderCurrentWeatherData(currentWeatherData) {
-    // create an element for each piece of valid data per instructions
-var dateTimeStamp = currentWeatherData.list [0].dt
-console.log(dateTimeStamp);
+    var dateTimeStamp = currentWeatherData.list[0].dt;
+    var temperature = currentWeatherData.list[0].main.temp;
+    var humidity = currentWeatherData.list[0].main.humidity;
+    var weatherDescription = currentWeatherData.list[0].weather[0].description;
+
+    var date = new Date(dateTimeStamp * 1000);
+    var formattedDate = date.toLocaleDateString();
+
+    cityNameElement.textContent = `Weather in ${currentWeatherData.city.name}`;
+    weatherDescriptionElement.textContent = `Description: ${weatherDescription}`;
+    temperatureElement.textContent = `Temperature: ${temperature}°F`;
+    humidityElement.textContent = `Humidity: ${humidity}%`;
 }
